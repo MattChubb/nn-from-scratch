685d9a84785f115b76308de1fb9bde52b8fb50e7
Colin Robertson <colin.robertson@zpg.co.uk>
2018-02-07 14:07:12 +0000

    Run over HTTP rather than HTTPS

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 uid=$(id -u)
 gid=$(id -g)
 
-docker run --rm -p 8888:8888 -v $PWD:/home/jovyan/work -e GEN_CERT=yes -e NB_UID=$uid -e NB_GID=$gid jupyter/scipy-notebook
+docker run --rm -p 8888:8888 -v $PWD:/home/jovyan/work -e NB_UID=$uid -e NB_GID=$gid  jupyter/scipy-notebook start-notebook.sh --NotebookApp.token=''
